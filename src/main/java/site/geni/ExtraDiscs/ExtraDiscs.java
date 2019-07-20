146e2ddc2f4f65e09964fab6b0e7ea231698b98c
geni <me@geni.site>
2019-07-21 00:09:36 +0300

    use fabric loot-table API instead of modifying vanilla loot table, update to 1.14

--- src/main/java/site/geni/ExtraDiscs/ExtraDiscs.java
+++ src/main/java/site/geni/ExtraDiscs/ExtraDiscs.java
@@ -2,9 +2,14 @@ package site.geni.ExtraDiscs;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.LootJsonParser;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.loot.*;
+import net.minecraft.world.loot.entry.LootEntry;
 import org.apache.logging.log4j.LogManager;
 import site.geni.ExtraDiscs.init.ExtraDiscsItems;
 
@@ -22,5 +27,22 @@ public class ExtraDiscs implements ModInitializer {
 		ExtraDiscsItems.init();
 
 		LogManager.getLogger("ExtraDiscs").info("[ExtraDiscs] Initialized");
+
+		LootTableLoadingCallback.EVENT.register((resourceManager, manager, id, supplier, setter) -> {
+			if (id.toString().equals("minecraft:chests/simple_dungeon")) {
+				LootEntry swedenLootEntry = LootJsonParser.read("{\"type\":\"minecraft:item\",\"weight\":15,\"name\":\"extradiscs:music_disc_sweden\"}", LootEntry.class);
+				LootEntry ariaMathLootEntry = LootJsonParser.read("{\"type\":\"minecraft:item\",\"weight\":15,\"name\":\"extradiscs:music_disc_aria_math\"}", LootEntry.class);
+				LootEntry emptyLootEntry = LootJsonParser.read("{\"type\":\"empty\",\"weight\":70}", LootEntry.class);
+
+				LootPool pool = FabricLootPoolBuilder.builder()
+					.withEntry(swedenLootEntry)
+					.withEntry(ariaMathLootEntry)
+					.withEntry(emptyLootEntry)
+					.withRolls(UniformLootTableRange.between(1, 3))
+					.build();
+
+				supplier.withPool(pool);
+			}
+		});
 	}
 }
--- src/main/java/site/geni/ExtraDiscs/init/ExtraDiscsItems.java
+++ src/main/java/site/geni/ExtraDiscs/init/ExtraDiscsItems.java
@@ -8,7 +8,7 @@ import site.geni.ExtraDiscs.ExtraDiscs;
 import site.geni.ExtraDiscs.item.ModMusicDiscItem;
 
 public class ExtraDiscsItems {
-	private static final Item.Settings SETTINGS = new Item.Settings().stackSize(1).itemGroup(ExtraDiscs.ITEM_GROUP).rarity(Rarity.RARE);
+	private static final Item.Settings SETTINGS = new Item.Settings().maxCount(1).group(ExtraDiscs.ITEM_GROUP).rarity(Rarity.RARE);
 
 	public static Item MUSIC_DISC_MINECRAFT;
 	public static Item MUSIC_DISC_CLARK;
